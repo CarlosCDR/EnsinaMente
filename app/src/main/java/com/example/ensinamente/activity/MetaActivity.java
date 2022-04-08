1cfdb47f12e3106c871b130ec8fa5ef2c1724016
Carlos Daniel <carloscandidofpss@gmail.com>
2022-04-08 01:45:46 +0044

    Desenvolvimento do aplicativo Ensina Mente para o TCC, versão 0,7

---
+++ app/src/main/java/com/example/ensinamente/activity/MetaActivity.java
@@ -0,0 +1,16 @@
+package com.example.ensinamente.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ensinamente.R;
+
+public class MetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.ensinamente.R.layout.activity_meta);
+    }
+}
--- app/src/main/java/com/example/ensinamente/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/ensinamente/activity/PrincipalActivity.java
@@ -1,5 +1,6 @@
 package com.example.ensinamente.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.ensinamente.databinding.ActivityPrincipalBinding;
@@ -34,13 +35,20 @@ public class PrincipalActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        /*binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+
+    public void criarTarefa(View view){
+          startActivity(new Intent(this,TarefaActivity.class));
+    }
+    public void criarMeta(View view){
+         startActivity(new Intent(this,MetaActivity.class));
     }
 
     @Override
